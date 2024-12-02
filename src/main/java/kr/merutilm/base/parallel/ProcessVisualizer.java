e5b7b3e6367038b3867d1955b1570a39dc600b6b
tkdydwk7071 <merutilm@gmail.com>
2024-12-02 14:01:54 +0000

    refactored and added credits.txt

--- src/main/java/kr/merutilm/base/parallel/ProcessVisualizer.java
+++ src/main/java/kr/merutilm/base/parallel/ProcessVisualizer.java
@@ -4,4 +4,8 @@ import kr.merutilm.base.exception.IllegalRenderStateException;
 
 public interface ProcessVisualizer{
     void run(double progress) throws IllegalRenderStateException, InterruptedException;
+
+    static ProcessVisualizer na(){
+        return a -> {};
+    }
 }
