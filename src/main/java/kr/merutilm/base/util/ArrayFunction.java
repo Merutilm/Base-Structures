cb52a7032f413124da74c23bbd0b6ed13926228a
tkdydwk7071 <merutilm@gmail.com>
2024-12-29 12:55:45 +0000

    added arrayfunction exp2x

--- src/main/java/kr/merutilm/base/util/ArrayFunction.java
+++ src/main/java/kr/merutilm/base/util/ArrayFunction.java
@@ -122,7 +122,17 @@ public final class ArrayFunction {
         }
         return result;
     }
-
+    
+    public static long[] exp2xArr(long[] arr){
+        long[] arr2 = new long[arr.length * 2];
+        System.arraycopy(arr, 0, arr2, 0, arr.length);
+        return arr2;
+    }
+    public static int[] exp2xArr(int[] arr){
+        int[] arr2 = new int[arr.length * 2];
+        System.arraycopy(arr, 0, arr2, 0, arr.length);
+        return arr2;
+    }
     public static double[] exp2xArr(double[] arr){
         double[] arr2 = new double[arr.length * 2];
         System.arraycopy(arr, 0, arr2, 0, arr.length);
